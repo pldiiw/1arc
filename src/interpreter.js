9e365aa49e58d98455a39d7f62458d3ba7aff5ed
ehrakis <clement.grimaud@supinfo.com>
2017-03-19 12:04:55 +0100

    Add serialization function
    Ajoute la finction de serialization sui prend une string en paramètres,
    la string est coupée en partie de 4 charachtères chacuns puis mis dans
    un array qui est ensuite retourné

---
+++ src/interpreter.js
@@ -0,0 +1,15 @@
+var test_serialization = 'AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHH';
+console.log(serialization_layer(test_serialization));
+
+function serialization_layer(unserialized_string)
+{
+/*Take a string in input and return an array
+ *This function  cut the string into multiples string with 4 char in it*/
+	var serialized_instruction = [];
+	for (var i = 0; i < unserialized_string.length; i += 4)
+	{
+		//take 4 char in the string and add it to the array serialized_instruction
+		serialized_instruction.push(unserialized_string.substring(i,i+4));
+	};	
+	return serialized_instruction
+}
