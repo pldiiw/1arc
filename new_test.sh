cb4c7b875205497729d7b55fb661a6d410896e1a
PLD <pierre-louis@dourneau.me>
2017-03-22 13:49:23 +0100

    Make new_test.sh automatically prepend test number
    Previously, we had to specify the test number in the test name when
    invoking new_test.sh, like so: `./new_test.sh 00-engine js`. Now we can
    just write `./new_test.sh engine js` and the script will the prepend the
    test number depending on the count of files having two figures number at
    the start of their names inside the test/ directory.

--- new_test.sh
+++ new_test.sh
@@ -3,9 +3,14 @@
 set -e
 
 if [ "$1" -a "$2" ]; then
-  touch test/$1.{$2,expected}
-  ${EDITOR:-true} test/$1.{$2,expected}
-  echo test/$1.{$2,expected}
+  testnum="$(ls test |
+    grep -oE '^[0-9]{2}' |
+    uniq |
+    wc -l |
+    xargs printf '%02d')"
+  touch test/$testnum-$1.{$2,expected}
+  ${EDITOR:-true} test/$testnum-$1.{$2,expected}
+  echo test/$testnum-$1.{$2,expected}
 else
   echo "Usage: ./new_test.sh <test-name> <test-ext>"
 fi
