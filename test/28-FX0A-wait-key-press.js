10b73876abcee84d4d305f56d1efb239dd24e148
PLD <pierre-louis@dourneau.me>
2017-04-10 12:34:48 +0200

    Improve tests for instruction FX0A.
    In the previous tests, the behaviour of the instruction when no key was
    pressed was not yet defined. Now, in a such case, the FX0A instruction
    should decrement the engine's program counter in order to make the next
    engine cycle re-read the FX0A instruction, until a key is pressed.

--- test/28-FX0A-wait-key-press.js
+++ test/28-FX0A-wait-key-press.js
@@ -1,12 +1,21 @@
 'use strict';
 
-// This test will need to be reviewed.
+// No key pressed? Decrement program counter in order to re-read the
+// waitKeyPress instruction.
+// Key pressed? Apply normal behaviour: save key value into register.
 
 let engine = require('engine').initialize();
 const waitKeyPress = require('instructions').waitKeyPress;
 
-engine.keypad[7] = 1;
+engine.data[0xB] = 230;
 
+engine.pc = 4000;
 const engine1 = waitKeyPress(engine, 0xB);
 
+engine.keypad[7] = 1;
+const engine2 = waitKeyPress(engine, 0xB);
+
 console.log(engine1.data[0xB]);
+console.log(engine1.pc);
+console.log(engine2.data[0xB]);
+console.log(engine2.pc);
