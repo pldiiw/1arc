ad850fb8301130d28ed7791aea7149c5c3c57d36
PLD <pierre-louis@dourneau.me>
2017-05-01 17:41:05 +0200

    Migrate engine prepare tests to Jest

--- test/05-engine-prepare.js
+++ test/05-engine-prepare.js
@@ -1,17 +1,7 @@
-'use strict';
-
-binToDec = s => parseInt(s, 2);
-hex = n => n.toString(16);
-
+const engine = require('../src/engine.js').initialize();
+const prepare = require('../src/engine.js').prepare;
 const program = '11112222';
-const preparedEngine = require('engine').prepare(
-  require('engine').initialize(), program);
 
-const loadedProgram = hex(binToDec(
-  preparedEngine.memory.slice(512 * 8, 516 * 8 + 1)));
-const fontLoaded = hex(binToDec(
-  preparedEngine.memory.slice(0, 5 * 8 + 1))) === "F0909090F0";
-
-console.log(`Font loaded: seems to be ${fontLoaded}`);
-console.log(`Loaded program: ${loadedProgram}`);
-console.log(`Program counter: ${binToDec(preparedEngine.pc)}`);
+test('program counter set to 0x200', () => {
+  expect(prepare(engine, program).get('pc')).toBe(0x200);
+});
