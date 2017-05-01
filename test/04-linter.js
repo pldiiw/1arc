f517d06e74db86a3ffd83dfdd046d041c7884a9c
PLD <pierre-louis@dourneau.me>
2017-05-01 18:21:32 +0200

    Migrate linter tests to Jest

--- test/04-linter.js
+++ test/04-linter.js
@@ -1,15 +1,21 @@
-'use strict';
-
 const lint = require('utility').lint;
-
 const goodProgram = '611181107111C1111111A111F11ED111';
-console.log(lint(goodProgram));
-
 const secondGoodProgram = '811EB111211100EE00E0F129';
-console.log(lint(secondGoodProgram));
-
 const badProgram = goodProgram + 'F132';
-console.log(lint(badProgram));
-
 const secondBadProgram = secondGoodProgram + 'FG55';
-console.log(lint(secondBadProgram));
+
+test('let good program pass', () => {
+  expect(lint(goodProgram)).toBe(goodProgram);
+});
+
+test('let second good program pass', () => {
+  expect(lint(secondGoodProgram)).toBe(secondGoodProgram);
+});
+
+test('do not let bad program pass', () => {
+  expect(lint(badProgram)).toBe(null);
+});
+
+test('do not let second bad program pass', () => {
+  expect(lint(secondBadProgram)).toBe(null);
+});
