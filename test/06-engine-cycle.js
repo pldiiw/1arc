8938cefcd16b775aa50acdef376a23f54fb649c6
PLD <pierre-louis@dourneau.me>
2017-05-01 18:14:49 +0200

    Migrate engine cycle tests to Jest

--- test/06-engine-cycle.js
+++ test/06-engine-cycle.js
@@ -1,89 +1,168 @@
-'use strict';
-
-// The heavy use of const is intended, for testing the purity of each cycle.
-
-const log = console.log;
-const binToDec = s => parseInt(s, 2);
-
-const sumRegisters = engine => {
-  return engine.data.reduce((a, v) => a + binToDec(v), 0);
-};
-const logPc = engine => {
-  log(`Program counter decimal value: ${binToDec(engine.pc)}`);
-};
-const logRegSum = engine => {
-  log(`Registers decimal sum value: ${sumRegisters(engine)}`);
-};
-const logTimer = engine => {
-  log(`Timer decimal value: ${binToDec(engine.timer)}`);
-};
-const logSound = engine => {
-  log(`Sound timer decimal value: ${binToDec(engine.sound)}`);
-};
-
-const engine = require('engine');
-
+let engine = require('../src/engine.js').initialize();
+const prepare = require('../src/engine.js').prepare;
+const cycle = require('../src/engine.js').cycle;
 const program = '6014710AF1157203F218';
 
-log('Init');
-const engineState = engine.prepare(engine.initialize(), program);
-logPc(engineState);
-logRegSum(engineState);
+describe('engine set up', () => {
+  engine = prepare(engine, program);
 
-log(`\nCycle 1`);
-const engineState1 = engine.cycle(engineState);
-logPc(engineState1);
-logRegSum(engineState1);
-log(`0 register decimal value: ${binToDec(engineState1.data[0])}`);
+  test('program counter is set to 0x200', () => {
+    expect(engine.get('pc')).toBe(0x200);
+  });
+});
 
-log(`\nCycle 2`);
-const engineState2 = engine.cycle(engineState1);
-logPc(engineState2);
-logRegSum(engineState2);
-log(`1 register decimal value: ${binToDec(engineState2.data[1])}`);
+describe('cycle 1', () => {
+  engine = cycle(engine);
 
-log(`\nCycle 3`);
-const engineState3 = engine.cycle(engineState2);
-logPc(engineState3);
-logTimer(engineState3);
+  test('program counter is set to 0x202', () => {
+    expect(engine.get('pc')).toBe(0x202);
+  });
 
-log(`\nCycle 4`);
-const engineState4 = engine.cycle(engineState3);
-logPc(engineState4);
-logRegSum(engineState4);
-log(`2 register decimal value: ${binToDec(engineState2.data[2])}`);
-logTimer(engineState4);
+  test('register 0 is set to 20', () => {
+    expect(engine.get('data')[0]).toBe(20);
+  });
+});
 
-log(`\nCycle 5`);
-const engineState5 = engine.cycle(engineState4);
-logPc(engineState5);
-logTimer(engineState5);
-logSound(engineState5);
+describe('cycle 2', () => {
+  engine = cycle(engine);
 
-log(`\nCycle 6`);
-const engineState6 = engine.cycle(engineState5);
-logPc(engineState6);
-logTimer(engineState6);
-logSound(engineState6);
+  test('program counter is set to 0x204', () => {
+    expect(engine.get('pc')).toBe(0x204);
+  });
 
-log(`\nCycle 7`);
-const engineState7 = engine.cycle(engineState6);
-logPc(engineState7);
-logTimer(engineState7);
-logSound(engineState7);
+  test('register 0 is set to 20', () => {
+    expect(engine.get('data')[0]).toBe(20);
+  });
 
-log(`\nCycle 8`);
-const engineState8 = engine.cycle(engineState7);
-logPc(engineState8);
-logTimer(engineState8);
-logSound(engineState8);
+  test('register 1 is set to 10', () => {
+    expect(engine.get('data')[1]).toBe(10);
+  });
+});
 
-log(`\nCycle 9`);
-const engineState9 = engine.cycle(engineState8);
-logPc(engineState9);
-logRegSum(engineState9);
-log(`0 register decimal value: ${binToDec(engineState1.data[0])}`);
-log(`1 register decimal value: ${binToDec(engineState2.data[1])}`);
-log(`2 register decimal value: ${binToDec(engineState2.data[2])}`);
-logTimer(engineState9);
-logSound(engineState9);
+describe('cycle 3', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x206', () => {
+    expect(engine.get('pc')).toBe(0x206);
+  });
+
+  test('timer is set to 10', () => {
+    expect(engine.get('timer')).toBe(10);
+  });
+});
+
+describe('cycle 4', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x208', () => {
+    expect(engine.get('pc')).toBe(0x208);
+  });
+
+  test('register 0 is set to 20', () => {
+    expect(engine.get('data')[0]).toBe(20);
+  });
+
+  test('register 1 is set to 10', () => {
+    expect(engine.get('data')[1]).toBe(10);
+  });
+
+  test('register 2 is set to 5', () => {
+    expect(engine.get('data')[2]).toBe(5);
+  });
+
+  test('timer is set to 9', () => {
+    expect(engine.get('timer')).toBe(9);
+  });
+});
+
+describe('cycle 5', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x20A', () => {
+    expect(engine.get('pc')).toBe(0x20A);
+  });
+
+  test('timer is set to 8', () => {
+    expect(engine.get('timer')).toBe(8);
+  });
+
+  test('sound is set to 3', () => {
+    expect(engine.get('sound')).toBe(3);
+  });
+});
+
+describe('cycle 6', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x20C', () => {
+    expect(engine.get('pc')).toBe(0x20C);
+  });
+
+  test('timer is set to 7', () => {
+    expect(engine.get('timer')).toBe(7);
+  });
+
+  test('sound is set to 2', () => {
+    expect(engine.get('sound')).toBe(2);
+  });
+});
+
+describe('cycle 7', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x20E', () => {
+    expect(engine.get('pc')).toBe(0x20E);
+  });
+
+  test('timer is set to 6', () => {
+    expect(engine.get('timer')).toBe(6);
+  });
+
+  test('sound is set to 1', () => {
+    expect(engine.get('sound')).toBe(1);
+  });
+});
+
+describe('cycle 8', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x210', () => {
+    expect(engine.get('pc')).toBe(0x210);
+  });
+
+  test('timer is set to 5', () => {
+    expect(engine.get('timer')).toBe(5);
+  });
+
+  test('sound is set to 0', () => {
+    expect(engine.get('sound')).toBe(0);
+  });
+});
+
+describe('cycle 9', () => {
+  engine = cycle(engine);
+
+  test('program counter is set to 0x212', () => {
+    expect(engine.get('pc')).toBe(0x212);
+  });
+
+  test('register 0 is set to 20', () => {
+    expect(engine.get('data')[0]).toBe(20);
+  });
+
+  test('register 1 is set to 10', () => {
+    expect(engine.get('data')[1]).toBe(10);
+  });
+
+  test('register 2 is set to 5', () => {
+    expect(engine.get('data')[2]).toBe(5);
+  });
+
+  test('timer is set to 4', () => {
+    expect(engine.get('timer')).toBe(4);
+  });
+
+  test('sound is set to 0', () => {
+    expect(engine.get('sound')).toBe(0);
+  });
+});
