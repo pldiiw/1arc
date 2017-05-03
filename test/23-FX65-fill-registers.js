c515a744c8838797e3b9e151c389563dee6f37b2
PLD <pierre-louis@dourneau.me>
2017-05-03 14:14:55 +0200

    Migrate tests for instructions FX65 and FX33 to Jest

--- test/23-FX65-fill-registers.js
+++ test/23-FX65-fill-registers.js
@@ -1,16 +1,15 @@
-'use strict';
+const engine = require('../src/engine.js').initialize();
+const fillRegisters = require('../src/instruction-set.js').fillRegisters;
 
-let engine = require('engine').initialize();
-const fillRegisters = require('instructions').fillRegisters;
+test('fill reg 0 through 2 from memory', () => {
+  let memory = engine.get('memory');
+  memory[32] = 43;
+  memory[33] = 255;
+  memory[34] = 6;
+  const engine_ = fillRegisters(engine.set('memory', memory).set('I', 32), 2);
 
-engine.memory[32] = 43;
-engine.memory[33] = 255;
-engine.memory[34] = 6;
-engine.I = 32;
-
-const engine1 = fillRegisters(engine, 2);
-
-console.log(engine1.data[0]);
-console.log(engine1.data[1]);
-console.log(engine1.data[2]);
-console.log(engine1.I);
+  expect(engine_.get('data')[0]).toBe(43);
+  expect(engine_.get('data')[1]).toBe(255);
+  expect(engine_.get('data')[2]).toBe(6);
+  expect(engine_.get('I')).toBe(35);
+});
