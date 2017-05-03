a2c68c28280e74ce1b77b221fea5fbe2d5354e90
PLD <pierre-louis@dourneau.me>
2017-05-03 23:25:11 +0200

    run_tests.sh: Fine grain semistandard error filtering for tests
    And remove exit clause, that would always run when testing, thus
    blocking the suite of script.

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,9 @@
 # Linting
 semistandard src/**/*.js || exit
 semistandard test/*.js |
+  grep -v "'describe' is not defined" |
   grep -v "'test' is not defined" |
-  grep -v "'expect' is not defined" || exit
+  grep -v "'expect' is not defined"
 
 # Run JS tests
 jest test/
