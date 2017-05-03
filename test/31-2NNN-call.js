a6060b46386a38c1c1a205d0a02eeabcaa397804
PLD <pierre-louis@dourneau.me>
2017-05-03 16:24:15 +0200

    test/31-2NNN-call.js: Fix stored memory address in stack

--- test/31-2NNN-call.js
+++ test/31-2NNN-call.js
@@ -5,6 +5,6 @@ test('call subroutine located at address 500', () => {
   const engine_ = call(engine.set('pointer', 4).set('pc', 100), 500);
 
   expect(engine_.get('pointer')).toBe(5);
-  expect(engine_.get('stack')[4]).toBe(102);
+  expect(engine_.get('stack')[4]).toBe(100);
   expect(engine_.get('pc')).toBe(500);
 });
