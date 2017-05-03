cf93319270388456eec9f8a44c65b924da73dc56
PLD <pierre-louis@dourneau.me>
2017-05-03 17:26:55 +0200

    Improve tests for instruction DXYN (drawSprite)
    Now check for the overwrite flag in reg 0xF.
    Add two new tests: one to check that pixels are XORed when overwritten
    and one to check that when drawing outside of the display, it wraps the
    drawing at the opposite side of the display.

--- test/26-DXYN-draw-sprite.js
+++ test/26-DXYN-draw-sprite.js
@@ -18,4 +18,34 @@ test('draw nice underscore-cross sprite', () => {
     .toBe('false false false false true true true false');
   expect(engine_.get('display')[6].slice(10, 19).join(' '))
     .toBe('true true true true false true false false');
+  expect(engine_.get('data')[0xF]).toBe(0);
+});
+
+test('pixels are XORed when overwritten', () => {
+  let memory = engine.get('memory');
+  memory[3000] = 0x5A;
+  let display = engine.get('display');
+  display[0] = Array(8).fill(true).concat(display[0].slice(9));
+  const engine_ = drawSprite(engine.set('memory', memory)
+    .set('display', display).set('I', 3000), 0, 0, 1);
+
+  expect(engine_.get('display')[0].slice(0, 9).join(' '))
+    .toBe('true false true false false true false true');
+  expect(engine_.get('data')[0xF]).toBe(1);
+});
+
+test('wrap to the opposite side of the screen when outside', () => {
+  let data = engine.get('data');
+  data[0xD] = 20;
+  data[0xE] = 61;
+  let memory = engine.get('memory');
+  memory[0] = 0xFF;
+  const engine_ = drawSprite(engine.set('data', data).set('memory', memory),
+    0xD, 0xE, 1);
+
+  expect(engine_.get('display')[20].slice(0, 5).every(v => v === true))
+    .toBe(true);
+  expect(engine_.get('display')[20].slice(61).every(v => v === true))
+    .toBe(true);
+  expect(engine_.get('data')[0xF]).toBe(0);
 });
