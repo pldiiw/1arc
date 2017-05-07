b6a309fa4de874a1b26bc6bd54e02f001617567b
PLD <pierre-louis@dourneau.me>
2017-05-07 17:28:59 +0200

    Fix engine init tests and program loading tests
    Change the default jest regex to include all *.js files inside test/
    directory.
    In the 00-engine-initialization.js tests, some value for checking that
    an engine component is X bytes long were wrong. This is fixed.
    In the 03-program-loading.js tests, there were an error of memory
    parsing when comparing the program to load and it representation inside
    the memory. Also, in src/engine.js, a wrong index inside the
    loadProgram() was used to go through the program. This is now fixed and
    program loading tests now pass.

--- test/03-program-loading.js
+++ test/03-program-loading.js
@@ -3,9 +3,8 @@ const loadProgram = require('../src/engine.js').loadProgram;
 const program = '00001111222233334444AAAAFFFF';
 
 test('program loaded from address 0x200', () => {
-  expect(loadProgram(engine, program)
-    .get('memory')
+  expect(Array.from(loadProgram(engine, program).get('memory'))
     .slice(0x200, 0x200 + program.length / 2)
-    .map(v => v.toString(16).padStart(2, '0'))
+    .map(v => v.toString(16).padStart(2, '0').toUpperCase())
     .join('')).toBe(program);
 });
