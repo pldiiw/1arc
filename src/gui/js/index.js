7bff29bf65d6ae50fdad78fc8e4d1dfae4b06813
ehrakis <clement.grimaud@supinfo.com>
2017-06-05 14:51:57 +0200

    index.js: delete sleep function

--- src/gui/js/index.js
+++ src/gui/js/index.js
@@ -224,15 +224,6 @@ function UIKeypadUpdate () {
 
 }
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
 function defineInputs () {
   let down = false;
   keyPress = []
